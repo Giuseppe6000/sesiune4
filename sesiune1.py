7c7da1183e8a1dbe61a8c90aa6957079ea7c0a98
Giuseppe6000 <cristi.gabriel04@gmail.com>
2025-05-22 23:32:10 +0300

    tema

---
+++ sesiune1.py
@@ -0,0 +1,115 @@
+'''suma = int(input(f'introdu suma dorita: '))
+
+bancnota500 = suma // 500
+rest500 = suma % 500
+
+bancnota200 = rest500 // 200
+rest200 = rest500 % 200
+
+bancnota100 = rest200 // 100
+rest100 = rest200 % 100
+
+bancnota50 = rest100 // 50
+rest50 = rest100 % 50
+
+bancnota10 = rest50 // 10
+rest10 = rest50 % 10
+
+bancnota5 = rest10 // 5
+rest5 = rest10 % 5
+
+bancnota1 = rest5 // 1
+rest1 = rest5 % 1
+
+print(f'bancnote de 500: {bancnota500}')
+print(f'bancnote de 200: {bancnota200}')
+print(f'bancnote de 100: {bancnota100}')
+print(f'bancnote de 50: {bancnota50}')
+print(f'bancnote de 10: {bancnota10}')
+print(f'bancnote de 5: {bancnota5}')
+print(f'bancnote de 1: {bancnota1}'
+
+'''''''pret_init=float(input('Pretul tau:'))
+TVA = float(input('TVA:'))
+
+pret_tva=pret_init*TVA/100
+pret_fin = pret_init+pret_tva
+print(f'Pretul initial este {pret_init}')
+print(f'TVA-ul este: {TVA}')
+print(f'Pretul final cu TVA este: {pret_fin}'''
+
+'''nota_test = float(input(f'Nota de la test: '))
+nota_proiect = float(input(f'Nota de la proiect: '))
+nota_prezenta = float(input(f'Nota de la prezenta: '))
+
+nota_fin = nota_test * 0,3 + nota_proiect * 0.4 + nota_prezenta * 0.3
+
+
+print(f'Nota finala: {nota_fin}')'''
+
+
+'''numar1 = float(input(f'Care este primul numar: '))
+numar2 = float(input(f'Care este al doilea numar: '))
+
+adunare = numar1 + numar2
+scadere = numar1 - numar2
+inmultirea = numar1 * numar2
+impartire = numar1 / numar2
+impartire_intreaga = numar1 // numar2
+modulo = numar1 % numar2
+putere = numar1 ** numar2
+
+#print(f'adunare: {adunare}\n{scadere}\n{inmultirea}\n{impartire}\n{impartire_intreaga}\n{modulo}\n{putere}')
+
+print(type(adunare))'''
+
+'''numar1 = float(input('primul numar: '))
+numar2 = float(input('al doilea numar: '))
+numar3 = float(input('Al treilea numar: '))
+
+media_aritmetica = (numar1 + numar2 + numar3) / 3
+
+print(f'media aritmetica este: {media_aritmetica: 2f}'''
+
+'''varsta = float(input('Varsta: '))
+calcul_varsta = 2025 - varsta
+
+print(f'Varsta mea: {calcul_varsta}'''
+
+'''numar1 = float(input('primul numar: '))
+numar2 = float(input('al doilea numar: '))
+numar3 = float(input('al treilea numar: '))
+numar4 = float(input('al patrulea numar: '))
+
+calcul_aritmetic = (numar1 - numar4) * (numar3 + numar2) ** 2
+
+print(f'Rezultat: {calcul_aritmetic}')'''
+
+'''numar1 = float(input('primul numar: '))
+numar2 = float(input('al doilea numar: '))
+
+print(f'adunare: {numar1 + numar2}')
+print(f'scadere: {numar1 - numar2}')
+print(f'impartire: {numar1 / numar2}')
+print(f'inmultire: {numar1 * numar2}')
+print(f'impartire_totala: {numar1 // numar2}')
+print(f'modulo: {numar1 % numar2}')
+print(f'putere: {numar1 ** numar2}')'''
+
+'''numar1 = float(input('primul numar: '))
+numar2 = float(input('al doilea numar: '))
+
+calcul_matematic = (numar2 ** 2 + numar1 ** 2) * (numar2 ** 2 - numar1 ** 2)
+
+print(f'rezultat: {calcul_matematic}')'''
+
+
+
+
+
+
+
+
+
+
+
